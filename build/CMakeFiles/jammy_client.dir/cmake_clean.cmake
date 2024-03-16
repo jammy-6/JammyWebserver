ed7480b75a392b12f219743ea706e0a255a4ec79
jammy <920817290@qq.com>
2024-03-16 21:04:06 +0800

    使用CMake编译工具

---
+++ build/CMakeFiles/jammy_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jammy_client.dir/src/Buffer.cpp.o"
+  "CMakeFiles/jammy_client.dir/src/epoll_client.cpp.o"
+  "jammy_client"
+  "jammy_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jammy_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/jammy_server.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jammy_server.dir/src/Acceptor.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/Buffer.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/Channel.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/Connection.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/EchoServer.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/Epoll.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/EpollLoop.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/InetAddr.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/Socket.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/TcpServer.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/ThreadPool.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/TimeStamp.cpp.o"
+  "CMakeFiles/jammy_server.dir/src/epoll_server.cpp.o"
+  "jammy_server"
+  "jammy_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jammy_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
